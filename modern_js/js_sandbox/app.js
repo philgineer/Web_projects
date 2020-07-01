478fd742c3f8c33f80c862008806935e77084c5d
Philgineer <43261253+yunjh0420@users.noreply.github.com>
2020-07-02 01:36:30 +0900

    new project

--- modern_js/js_sandbox/app.js
+++ modern_js/js_sandbox/app.js
@@ -1,27 +1,9 @@
-let day;
+const user = {
+  first: "jon",
+  last: "doe",
+  age: 30,
+};
 
-switch (5) {
-  case 0:
-    day = "SUN";
-    break;
-  case 1:
-    day = "MON";
-    break;
-  case 2:
-    day = "TUE";
-    break;
-  case 3:
-    day = "WED";
-    break;
-  case 4:
-    day = "THU";
-    break;
-  case 5:
-    day = "FRI";
-    break;
-  case 6:
-    day = "SAT";
-    break;
+for (let x in user) {
+  console.log(x, user[x]);
 }
-
-console.log(day);
