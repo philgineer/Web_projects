c8713f64989b596762d0bca8ad5ecd43f5df1c86
Philgineer <43261253+yunjh0420@users.noreply.github.com>
2020-07-17 00:25:21 +0900

    fix url

--- vanilla_javascript_projects/11. Infinite Scroll Posts (Fetch, Async, Await, CSS Loader)/script.js
+++ vanilla_javascript_projects/11. Infinite Scroll Posts (Fetch, Async, Await, CSS Loader)/script.js
@@ -8,7 +8,7 @@ let page = 1;
 // Fetch posts from API
 async function getPosts() {
   const res = await fetch(
-    `http://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
   );
 
   const data = await res.json();
